9f5b382712ce2b28b062d38393ecbb448c232476
Big Head <spencer.moyer16@yahoo.com>
2021-04-22 16:14:31 -0400

    seeds are failing

--- db/schema.sql
+++ db/schema.sql
@@ -19,8 +19,8 @@ CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
+    role_id INTEGER NOT NULL,
     manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO departments (name)
+VALUES
+    ('Engineering'), ('Sales'), ('Accountant'), ('Marketing');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Chief Technology Officer', 175000, 1),
+    ('Lead Salesman', 110000, 2),
+    ('Lead Marketer', 120000, 4),
+    ('Senior Engineer', 130000, 1),
+    ('Junior Engineer', 110000, 1)
+    ('Salesperson', 85000, 2),
+    ('Accountant', 90000, 3),
+    ('Marketing Rep', 95000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Spencer', 'Moyer', 1, NULL),
+    ('John', 'Doe', 2, NULL),
+    ('Emily', 'Byer', 3, NULL),
+    ('Random', 'Name', 8, 3),
+    ('Hello', 'There', 4, 1),
+    ('Another', 'Random', 7, NULL),
+    ('Cant', 'Think', 6, 2),
+    ('Frank', 'Smith', 5, 1);
